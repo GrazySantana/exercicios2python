3aaf9fce06bc77b5c83adb646f822384441eeaf6
GrazySantana <130664561+GrazySantana@users.noreply.github.com>
2024-08-09 09:02:39 -0300

    .
    .

---
+++ ex_02.py
@@ -0,0 +1,11 @@
+# Grazielly Cristine
+#Variaveis
+Pessoa1 = 1.58
+Pessoa2 = 1.20
+
+# Verificar valores
+if Pessoa1 > Pessoa2:
+    print("A pessoa1 é mais alta doque a Pessoa2. ")
+
+else: 
+    print("A pessoa2 é mais alta doque a Pessoa1. ")
