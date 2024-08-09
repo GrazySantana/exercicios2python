0251f1ec928ba3619f2725c607ff9ec2f20dc141
GrazySantana <130664561+GrazySantana@users.noreply.github.com>
2024-08-09 10:03:08 -0300

    .
    .

--- exercicio2.py
+++ exercicio2.py
@@ -5,7 +5,7 @@ Pessoa2 = 1.20
 
 # Verificar valores
 if Pessoa1 > Pessoa2:
-    print("A pessoa1 é mais alta doque a Pessoa2. ")
+    print("A pessoa1 é mais alta do que a Pessoa2. ")
 
 else: 
-    print("A pessoa2 é mais alta doque a Pessoa1. ")
+    print("A pessoa2 é mais alta do que a Pessoa1. ")
